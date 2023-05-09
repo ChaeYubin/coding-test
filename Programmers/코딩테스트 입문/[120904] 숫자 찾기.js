1bfa10cbda718d5b6b2125448641fc77e146735d
ChaeYubin <cyoobin@naver.com>
2023-05-09 21:14:38 +0900

    test

--- Programmers/코딩테스트 입문/[120904] 숫자 찾기.js
+++
@@ -1,4 +0,0 @@
-function solution(num, k) {
-  const arr = String(num).split("");
-  return arr.indexOf(String(k)) == -1 ? -1 : arr.indexOf(String(k)) + 1;
-}
--- Programmers/코딩테스트 입문/[120906] 자릿수 더하기.js
+++
@@ -1,5 +0,0 @@
-function solution(n) {
-  return String(n)
-    .split("")
-    .reduce((acc, cur) => acc + Number(cur), 0);
-}
